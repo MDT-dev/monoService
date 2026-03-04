40e5092fb63ca223c2b057de589574304a908d29
MDT-dev <manueldiogotomas@gmail.com>
2026-03-04 09:29:14 +0100

    fix: removed

--- src/app/produtos/page.tsx
+++ src/app/produtos/page.tsx
@@ -1,4 +1,5 @@
 import { ProdutosGrid } from '@/components/product_zone/produtos-grid';
+export const dynamic = 'force-dynamic'
 
 export default function ProdutosPage() {
   return <ProdutosGrid />;
